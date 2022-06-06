7e5172d5b9e282741a510d37a7aca683f047a169
HarukiZhang <harukizhang@outlook.com>
2022-06-07 01:29:57 +0800

    exercise B+ tree : not complete;

--- Cpp/zjdsa/pta_adsaa_7-3_self_printable_B_plus_tree.cpp
+++ Cpp/zjdsa/pta_adsaa_7-3_self_printable_B_plus_tree.cpp
@@ -2,31 +2,33 @@
 #include<queue>
 using namespace std;
 
-constexpr int kMaxLen = 10010, kMinDeg = 2, kMaxKey = 3, kMaxPtr = 4;
+constexpr int kMaxLen = 5, kMaxKey = 3, kMaxPtr = 4;
 
 typedef int KeyType;
 typedef struct Node *TreePtr;
 struct Node {
     int n = 0;
     bool isLeaf = true;
-    KeyType keys[kMaxPtr] = {-1, -1, -1, -1};
-    TreePtr ptrs[kMaxPtr] = {0, 0, 0, 0};
+    KeyType keys[kMaxLen] = {-1, -1, -1, -1, -1};
+    TreePtr parent = nullptr;
+    TreePtr ptrs[kMaxLen] = {0, 0, 0, 0, 0};
 };
 
 class BTree {
 public:
     BTree();
     ~BTree();
+    TreePtr search(KeyType key, int &idx);
     void insert(KeyType key);
     void self_print();
 private:
-    void _split(TreePtr x, int i);
-    void _insert_nonFull(TreePtr x, KeyType key);
+    void _split(TreePtr x);
     
     TreePtr root = nullptr;
 };
 
 int main(){
+    sizeof(Node);
     freopen("E:\\in.txt", "r", stdin);
     // ios::sync_with_stdio(false);
     int N, keyin;
@@ -58,85 +60,114 @@ BTree::~BTree(){
     }
 }
 
+TreePtr BTree::search(KeyType key, int &idx){
+    idx = -1;
+    TreePtr ptr = root;
+    while (ptr){
+        if (ptr->isLeaf){
+            for (idx = 1; idx <= ptr->n; ++idx)
+                if (ptr->keys[idx] == key)
+                    return ptr;
+            idx = -1;
+            return ptr;
+        }
+        idx = ptr->n;
+        while (key < ptr->keys[idx]) --idx;//key is all positive;
+        ptr = ptr->ptrs[idx];
+    }
+    return ptr;
+}
+
+
 void BTree::insert(KeyType key){
-    TreePtr r = root;
-    if (r->n == kMaxKey){
-        root = new Node;
-        root->isLeaf = false;
-        root->ptrs[0] = r;
-        _split(root, 0);
+    int idx = -1;
+    TreePtr ptr = search(key, idx);
+    if (idx == -1){
+        for (idx = ptr->n; key < ptr->keys[idx]; --idx)
+            ptr->keys[idx + 1] = ptr->keys[idx];
+        ptr->keys[idx + 1] = key;
+        ++ptr->n;
+        if (ptr->n == kMaxPtr)//ptr has to be a leaf node;
+            _split(ptr);
     }
-    _insert_nonFull(root, key);
+    else cout << "Key " << key << " is duplicated" << endl;
     return;
 }
 
-void BTree::_split(TreePtr x, int i){
+void BTree::_split(TreePtr x){
     TreePtr z = new Node;
-    TreePtr y = x->ptrs[i];
-    z->keys[1] = y->keys[3];
-    z->ptrs[0] = y->ptrs[2];
-    z->ptrs[1] = y->ptrs[3];
-    for (int j = x->n; j > i; --j){
-        x->keys[j + 1] = x->keys[j];
-        x->ptrs[j + 1] = x->ptrs[j];
+    int i = x->n;//i == 4 if isLeaf; i == 3 if nonLeaf;
+    for (; i > 2; --i, --x->n, ++z->n){
+        z->keys[i - 2] = x->keys[i];
+        z->ptrs[i - 2] = x->ptrs[i];
     }
-    x->keys[i + 1] = y->keys[2];//i max to 2;
-    x->ptrs[i + 1] = z;
-    ++x->n;
-    y->n = z->n = 1;
-    return;
-}
-/*
-if x->n == 0 && i == 0 : 
-    transfer y[2] to x[1];
-
-if x->n == 1 && i == 0 : 
-    move x[1] to x[2]; 
-    transfer y[2] to x[1];
-
-if x->n == 1 && i == 1 : 
-    transfer y[2] to x[2];
-    
-if x->n == 2 && i == 0 : 
-    move x[2] to x[3]; 
-    move x[1] to x[2]; 
-    transfer y[2] to x[1];
-
-if x->n == 2 && i == 1 : 
-    move x[2] to x[3]; 
-    transfer y[2] to x[2];
-
-if x->n == 2 && i == 2 : 
-    transfer y[2] to x[3];
-*/
-
-void BTree::_insert_nonFull(TreePtr x, KeyType key){
-    while (true){
-        int i = x->n;
+    z->ptrs[i - 2] = x->ptrs[i];//i == 2;
+    if (x == root){
+        TreePtr r = new Node;
+        r->isLeaf = false;
         if (x->isLeaf){
-            for (int j = 1; j <= x->n; ++j)
-                if (x->keys[j] == key){
-                    cout  << "Key " << key << " is duplicated" << endl;
-                    break;
-                }
-            for (; i >= 1 && key < x->keys[i]; --i)
-                x->keys[i + 1] = x->keys[i];
-            x->keys[i + 1] = key;
-            ++x->n;
-            break;
+            r->keys[1] = x->keys[3];
+            // z->keys[2] = x->keys[4];
+            // z->keys[1] = x->keys[3];
+            // x->n = z->n = 2;
         }
-        while (i >= 1 && key < x->keys[i]) --i;
-        if (i != 0 && key == x->keys[i]){
-            cout << "Key " << key << " is duplicated" << endl;
-            break;
+        else {
+            z->isLeaf = false;
+            r->keys[1] = x->keys[2];
+            // z->keys[1] = x->keys[3];
+            // z->ptrs[0] = x->ptrs[2];
+            // z->ptrs[1] = x->ptrs[3];
+            // x->n = z->n = 1;
         }
-        if (x->ptrs[i]->n == kMaxKey){
-            _split(x, i);
-            if (key >= x->keys[i + 1])
-                ++i;
-        }
-        x = x->ptrs[i];
+        r->ptrs[0] = x;
+        r->ptrs[1] = z;
+        r->n = 1;
+        x->parent = z->parent = r;
+        root = r;
+        return;
     }
+    int shift_up = x->isLeaf ? x->keys[3] : x->keys[2];
+    TreePtr p = x->parent;
+    i = p->n;
+    for (; shift_up < p->keys[i]; --i)
+        p->keys[i + 1] = p->keys[i];
+    p->keys[i + 1] = shift_up;
+    p->ptrs[i + 1] = z;
+    z->parent = p;
+    ++p->n;
+    if (p->n == kMaxKey) _split(p);    
+    // if (x->isLeaf){
+    //     // z->keys[2] = x->keys[4];
+    //     // z->keys[1] = x->keys[3];
+    //     TreePtr p = x->parent;
+    //     int i = p->n;
+    //     for (; z->keys[1] < p->keys[i]; --i)
+    //         p->keys[i + 1] = p->keys[i];
+    //     p->keys[i + 1] = z->keys[1];
+    //     p->ptrs[i + 1] = z;
+    //     z->parent = p;
+    //     //z->n has increased;
+    //     ++p->n;
+    //     if (p->n == kMaxKey) _split(p);
+    // }
+    // else {//x is an interior node;
+    //     z->isLeaf = false;
+    //     // z->keys[1] = x->keys[3];
+    //     // z->ptrs[0] = x->ptrs[2];
+    //     // z->ptrs[1] = x->ptrs[3];
+    //     TreePtr p = x->parent;
+    //     int i = p->n;
+    //     for (; x->keys[2] < p->keys[i]; --i){
+    //         p->keys[i + 1] = p->keys[i];
+    //         p->ptrs[i + 1] = p->ptrs[i];
+    //     }
+    //     p->keys[i + 1] = x->keys[2];
+    //     p->ptrs[i + 1] = z;
+    //     z->parent = p;
+    //     //z->n has increased;
+    //     ++p->n;
+    //     if (p->n == kMaxKey) _split(p);
+    // }
     return;
 }
 
@@ -153,7 +184,7 @@ void BTree::self_print(){
                 if (i != 1) cout << ",";
                 cout << crtP->keys[i];
             }
-            if (crtP->ptrs[i])
+            if (!crtP->isLeaf && crtP->ptrs[i])
                 que.push(last = crtP->ptrs[i]);
         }
         cout << "]";
